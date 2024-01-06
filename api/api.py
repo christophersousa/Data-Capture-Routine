d651cb934ce882d43246cd3ef5df639d7f13c05c
christopher-sousa <christopher.sousa@stage.consulting>
2024-01-06 17:53:33 -0300

    feat: get data

--- api/api.py
+++ api/api.py
@@ -2,17 +2,30 @@ import requests
 import credentials
 # Url and Headers
 headers = {"accept": "application/json"}
-url = f"{credentials.url}/deals"
 
 # Requests
 def request_deals(page:int, dateStart:str, dateEnd:str):
   print(f"Request deals page: {page}")
+  url = f"{credentials.url}/deals"
   params = {
     'token': credentials.token,
     'limit': 200,
     'page': page,
     'start_date': dateStart,
     'end_date': dateEnd
+  }
+  response = requests.get(url=url,headers=headers,params=params)
+  if(response.status_code != 200):
+    raise Exception('Invalid response in list_deals')
+  return response.json()
+
+def request_organization(page:int):
+  print(f"Request deals page: {page}")
+  url = f"{credentials.url}/organizations"
+  params = {
+    'token': credentials.token,
+    'limit': 200,
+    'page': page
   }
   response = requests.get(url=url,headers=headers,params=params)
   if(response.status_code != 200):
--- processing/requests_datas.py
+++ processing/requests_datas.py
@@ -1,4 +1,4 @@
-from api.api import request_deals
+from api.api import request_deals, request_organization
 def list_deals(dateStart:str, dateEnd:str):
   try:
     deals_list = []
@@ -19,5 +19,27 @@ def list_deals(dateStart:str, dateEnd:str):
     return deals_list
   except Exception as e:
     print(f"Error in request_deals. \nError: {e.message}")
+    raise e
+
+def list_organizations():
+  try:
+    organizations_list = []
+    page = 1
+    response = request_organization(page)
+    organizations_list.extend(response['organizations'])
+    if response['has_more']:
+      page += 1
+      while page <= 5:
+        try:
+          response = request_organization(page)
+          organizations_list.extend(response['organizations'])
+          if not response['has_more']:
+            break
+          page += 1
+        except Exception as e:
+          print(f'Error in request_organization in page {page}. \nError: {e}')
+    return organizations_list
+  except Exception as e:
+    print(f"Error in request_organization. \nError: {e.message}")
     raise e
 
